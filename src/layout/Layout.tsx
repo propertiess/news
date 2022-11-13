b9fc78a7d12253ef3bb78cc43d1645f0a99c2071
propertiess <erofeev2001@yandex.ru>
2022-11-13 20:09:44 +0300

    feat (Layout): add scroll to top every route

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,7 +1,14 @@
-import { FC, PropsWithChildren } from 'react';
+import { FC, PropsWithChildren, useLayoutEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Header, Main } from '@/components';
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Header />
