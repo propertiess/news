2538c58291b5a19aa465f6fdc578df35ce67c374
propertiess <erofeev2001@yandex.ru>
2022-11-13 21:42:06 +0300

    fix (useScrollView): scroll back, when preload

--- src/hooks/useScrollView.ts
+++ src/hooks/useScrollView.ts
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useActions, useAppSelector } from '@/store/hooks';
 
 export const useScrollView = () => {
@@ -8,19 +9,23 @@ export const useScrollView = () => {
     postsTop: top
   } = useAppSelector(state => state.scroll);
 
-  const { loading: isLoading } = useAppSelector(state => state.idPosts);
+  const { pathname } = useLocation();
+  const ref = useRef<string>('');
 
   const { setTop } = useActions();
 
   useEffect(() => {
-    if (isLoading) return;
-
-    if (currRenderedPostsCount === shouldRendered && top) {
+    if (
+      currRenderedPostsCount === shouldRendered &&
+      top &&
+      pathname !== ref.current
+    ) {
       window.scrollTo({
         top
       });
+      ref.current = pathname;
     }
-  }, [currRenderedPostsCount, top, isLoading]);
+  }, [currRenderedPostsCount, top, pathname]);
 
   const onClick = () => {
     setTop(window.scrollY);
